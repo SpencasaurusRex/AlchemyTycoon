e0836712082799cfb068d7e6c9f702e49fcaf64a
SpencasaurusRex <SpencasaurusRex@gmail.com>
2019-08-13 03:34:00 -0400

    Working on UI

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -1,23 +1,56 @@
-﻿public class Ingredient
-{
-    IngredientType Type;
-    float quality;
+﻿using UnityEngine;
 
-    public Ingredient(IngredientType type)
-    {
-        Type = type;
-    }
+public class Ingredient : MonoBehaviour
+{
+    public string Name;
+    public float[] Attributes;
 }
 
 public enum IngredientType
 {
-    ArrowRoot,
-    BlueToadshade,
-    BloodLeaf,
-    LavenderSprig,
-    WyrmtonguePetals,
+    MilkWeedSeeds,
     AmanitaCap,
-    CactusJuice,
-    MilkweedSeeds,
-    BloodGrass
+    ArrowRoot,
+    BloodGrass,
+    LavenderSprig,
+    BlueToadshade
+}
+
+public static class IngredientTypeExtensions
+{
+    static readonly int[][] IngredientAttributes =
+    {
+        new [] { 1, -1,  0},
+        new [] { 1,  0, -1},
+        new [] { 0,  1, -1},
+        new [] {-1,  1,  0},
+        new [] {-1,  0,  1},
+        new [] { 0, -1,  1},
+    };
+
+    public static int[] Attributes(this IngredientType type)
+    {
+        return IngredientAttributes[(int)type];
+    }
+
+    public static string Name(this IngredientType type)
+    {
+        switch (type)
+        {
+            case IngredientType.MilkWeedSeeds:
+                return "Milkweed Seeds";
+            case IngredientType.AmanitaCap:
+                return "Amanita Cap";
+            case IngredientType.ArrowRoot:
+                return "Arrowroot";
+            case IngredientType.BloodGrass:
+                return "Bloodgrass";
+            case IngredientType.LavenderSprig:
+                return "Lavender Sprig";
+            case IngredientType.BlueToadshade:
+                return "Blue Toadshade";
+            default:
+                return "Mixture";
+        }
+    }
 }
