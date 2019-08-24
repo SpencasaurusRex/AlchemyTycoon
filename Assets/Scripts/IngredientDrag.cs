cc266a0ada303ab9b2f4c1414276c2def56ce852
SpencasaurusRex <SpencasaurusRex@gmail.com>
2019-08-24 04:23:29 -0400

    Added new sprites

--- Assets/Scripts/IngredientDrag.cs
+++ Assets/Scripts/IngredientDrag.cs
@@ -19,6 +19,7 @@ public class IngredientDrag : MonoBehaviour
     { 
         if (!Dragging) return;
         transform.position = Vector3.Lerp(transform.position, MousePosition, 1 - Mathf.Exp(-Sharpness * Time.deltaTime));
+        Sharpness += Time.deltaTime * 10;
     }
 
     Vector3 MousePosition => Camera.main.ScreenToWorldPoint(Input.mousePosition).WithZ(0);
