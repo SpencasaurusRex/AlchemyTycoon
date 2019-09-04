70d2829512fcc22882d821940a9ee846926c8238
Spencer Haney <SpencasaurusRex@gmail.com>
2019-09-03 23:53:54 -0400

    Handle multiple cameras

--- Assets/Scripts/Framework/SceneObject.cs
+++ Assets/Scripts/Framework/SceneObject.cs
@@ -5,6 +5,8 @@ public class SceneObject : MonoBehaviour
 {
     SpriteRenderer sr;
     Canvas cv;
+    Camera cam;
+    AudioListener aud;
 
     [ShowInInspector]
     public bool Invisible { get; set; }
@@ -13,6 +15,8 @@ public class SceneObject : MonoBehaviour
     {
         sr = GetComponent<SpriteRenderer>();
         cv = GetComponent<Canvas>();
+        cam = GetComponent<Camera>();
+        aud = GetComponent<AudioListener>();
 
         GameController.Instance.Register(this);
     }
@@ -21,6 +25,8 @@ public class SceneObject : MonoBehaviour
     {
         if (sr != null) sr.enabled = !Invisible;
         if (cv != null) cv.enabled = !Invisible;
+        if (cam != null) cam.enabled = true;
+        if (aud != null) aud.enabled = true;
     }
 
     public void Hide(bool switchingScene = false)
@@ -37,5 +43,7 @@ public class SceneObject : MonoBehaviour
 
         if (sr != null) sr.enabled = false;
         if (cv != null) cv.enabled = false;
+        if (cam != null) cam.enabled = false;
+        if (aud != null) aud.enabled = false;
     }
 }
