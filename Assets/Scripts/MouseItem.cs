eac140a4796e3d0608036961593099754b6f4309
SpencasaurusRex <SpencasaurusRex@gmail.com>
2019-09-08 02:24:43 -0400

    Add grid highlight

---
+++ Assets/Scripts/MouseItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MouseItem : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition).WithZ(0);
+    }
+}
